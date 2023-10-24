4d0425d8c6a706a81809d0903b464eabf5ef846e
Leandro <devleandroanjos@gmail.com>
2023-10-23 22:50:55 -0300

    versão final do exercicio

--- src/PrimeiraClasse.java
+++ src/PrimeiraClasse.java
@@ -2,4 +2,22 @@
  * @author Leandro
  */
 public class PrimeiraClasse {
+    public static void main(String args []){
+        System.out.println("Começando o programa");
+        Produto produto = new Produto();
+        produto.setNome("Pedal");
+        produto.setCodigo(3559);
+        produto.setTipoembalagem("Caixa");
+        produto.setDimensoes("12x15x10cm");
+        String nomeProduto = produto.getNome();
+        int codigoProduto = produto.getCodigo();
+        String tipoEmbalagem = produto.getTipoembalagem();
+        String dimensoesProduto = produto.getDimensoes();
+        System.out.println("**************");
+        System.out.println("Informações sobre o " + nomeProduto);
+        System.out.println("Código do Produto: " + codigoProduto);
+        System.out.println("Tipo de embalagem: " + tipoEmbalagem);
+        System.out.println("Dimensões da embalagem " + dimensoesProduto);
+        System.out.println("**************");
+    }
 }
---
+++ src/Produto.java
@@ -0,0 +1,42 @@
+public class Produto {
+
+    private int codigo;
+
+    private String nome;
+
+    private String tipoembalagem;
+
+    private String dimensoes;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipoembalagem() {
+        return tipoembalagem;
+    }
+
+    public void setTipoembalagem(String tipoembalagem) {
+        this.tipoembalagem = tipoembalagem;
+    }
+
+    public String getDimensoes() {
+        return dimensoes;
+    }
+
+    public void setDimensoes(String dimensoes) {
+        this.dimensoes = dimensoes;
+    }
+}
